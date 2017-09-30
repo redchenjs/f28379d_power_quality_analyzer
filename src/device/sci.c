0a797b86e876e147e03c4999ec3ce427542b3d33
redchenjs <redchenjs@live.com>
2017-09-30 22:24:22 +0800

    fix sci io

--- src/device/sci.c
+++ src/device/sci.c
@@ -14,10 +14,6 @@
 #include <file.h>
 
 #include "F2837xD_device.h"
-#include "F2837xD_sci_io.h"
-
-volatile int scia_status = 0;
-volatile FILE *scia_fid;
 
 typedef struct sci_buff_def {
     uint16_t data;
@@ -43,6 +39,104 @@ uint16_t sci2_rx_num = 0;
 __interrupt void sci2_tx_fifo_isr(void);
 __interrupt void sci2_rx_fifo_isr(void);
 
+uint16_t scia_device_open = 0;
+
+volatile int scia_status = 0;
+volatile FILE *scia_fid;
+
+int scia_open(const char * path, unsigned int flags, int llv_fd)
+{
+    if (scia_device_open) {
+        return (-1);
+    } else {
+        EALLOW;
+        CpuSysRegs.PCLKCR7.bit.SCI_A = 1;
+        SciaRegs.SCIFFTX.all=0xE040;
+        SciaRegs.SCIFFRX.all=0x2044;
+        SciaRegs.SCIFFCT.all=0x0;
+        SciaRegs.SCICCR.all =0x0007;   // 1 stop bit,  No loopback
+                                       // No parity,8 char bits,
+                                       // async mode, idle-line protocol
+        SciaRegs.SCICTL1.all =0x0003;  // enable TX, RX, internal SCICLK,
+                                       // Disable RX ERR, SLEEP, TXWAKE
+        SciaRegs.SCICTL2.all =0x0003;
+        SciaRegs.SCICTL2.bit.TXINTENA =1;
+        SciaRegs.SCICTL2.bit.RXBKINTENA =1;
+
+        SciaRegs.SCIHBAUD.bit.BAUD =0x0000;  // 9600 baud @LSPCLK = 10MHz
+                                             //(40 MHz SYSCLK).
+        SciaRegs.SCILBAUD.bit.BAUD =0x0081;
+
+        SciaRegs.SCICTL1.all =0x0023;  // Relinquish SCI from Reset
+        EDIS;
+
+        scia_device_open = 1;
+        return (1);
+    }
+}
+
+int scia_close(int dev_fd)
+{
+    if ((dev_fd != 1) || (!scia_device_open)) {
+        return (-1);
+    } else {
+        scia_device_open = 0;
+        return (0);
+    }
+}
+
+int scia_read(int dev_fd, char * buf, unsigned int count)
+{
+    uint16_t readCount = 0;
+    uint16_t * bufPtr = (uint16_t *) buf;
+
+    if (count == 0) {
+        return (0);
+    }
+
+    while ((readCount < count) && SciaRegs.SCIRXST.bit.RXRDY) {
+        *bufPtr = SciaRegs.SCIRXBUF.bit.SAR;
+        readCount++;
+        bufPtr++;
+    }
+
+    return (readCount);
+}
+
+int scia_write(int dev_fd, const char * buf, unsigned int count)
+{
+    uint16_t writeCount = 0;
+    uint16_t * bufPtr = (uint16_t *) buf;
+
+    if (count == 0) {
+        return (0);
+    }
+
+    while (writeCount < count) {
+        while (SciaRegs.SCICTL2.bit.TXRDY != 1);
+        SciaRegs.SCITXBUF.bit.TXDT = *bufPtr;
+        writeCount++;
+        bufPtr++;
+    }
+
+    return (writeCount);
+}
+
+off_t scia_lseek(int dev_fd, off_t offset, int origin)
+{
+    return (0);
+}
+
+int scia_unlink(const char * path)
+{
+    return (0);
+}
+
+int scia_rename(const char * old_name, const char * new_name)
+{
+    return (0);
+}
+
 void sci1_init(void)
 {
     EALLOW;
@@ -55,8 +149,8 @@ void sci1_init(void)
     //
     // Redirect STDOUT to SCI
     //
-    scia_status = add_device("scia", _SSA, SCI_open, SCI_close, SCI_read, SCI_write,
-                        SCI_lseek, SCI_unlink, SCI_rename);
+    scia_status = add_device("scia", _SSA, scia_open, scia_close, scia_read, scia_write,
+                        scia_lseek, scia_unlink, scia_rename);
     scia_fid = fopen("scia","w");
     freopen("scia:", "w", stdout);
     setvbuf(stdout, NULL, _IONBF, 0);
